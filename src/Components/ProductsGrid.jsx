b00a6f67a97ef729ee3ce0e8b35769eea2ff5366
AmineAffif <affif.amine@live.fr>
2023-11-12 00:12:52 +0100

    feat: Added a loader when we wait for data to be displayed on home page

--- src/Components/ProductsGrid.jsx
+++ src/Components/ProductsGrid.jsx
@@ -5,14 +5,20 @@ import ProductCard from "./ProductCard"; // Un composant pour afficher chaque pr
 const ProductsGrid = () => {
   const [products, setProducts] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
+    setIsLoading(true); // Début du chargement
     axios
       .get("http://localhost:3000/products")
       .then((response) => {
         setProducts(response.data);
+        setIsLoading(false); // Fin du chargement
       })
-      .catch((error) => console.error("Error fetching data: ", error));
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+        setIsLoading(false); // Fin du chargement même en cas d'erreur
+      });
   }, []);
 
   return (
@@ -24,15 +30,20 @@ const ProductsGrid = () => {
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
       />
-      <div className="products-grid">
-        {products
-          .filter((product) =>
-            product.name.toLowerCase().includes(searchTerm.toLowerCase())
-          )
-          .map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-      </div>
+
+      {isLoading ? (
+        <div className="loader"></div> // Affichage du loader
+      ) : (
+        <div className="products-grid">
+          {products
+            .filter((product) =>
+              product.name.toLowerCase().includes(searchTerm.toLowerCase())
+            )
+            .map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+        </div>
+      )}
     </div>
   );
 };
