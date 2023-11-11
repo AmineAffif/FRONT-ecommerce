ac3734c22bd7020a2d66685791c7867ea944357c
AmineAffif <affif.amine@live.fr>
2023-11-11 21:18:03 +0100

    style: some responsive for homepage and some styling too

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -1,17 +1,31 @@
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
+import { Link } from "react-router-dom"; // Importer Link
 import { FontAwesomeIcon, faPlus } from "../icons";
-import { CartContext } from '../Context/CartContext';
+import { CartContext } from "../Context/CartContext";
 
 const ProductCard = ({ product }) => {
   const { addToCart } = useContext(CartContext);
+  // URL de l'image avec un paramètre unique pour chaque produit
+  const imageUrl = `https://picsum.photos/200/250?random=${product.id}`;
+
+  const handleAddToCart = (event) => {
+    event.stopPropagation(); // Empêche la redirection lors de l'ajout au panier
+    addToCart(product);
+  };
 
   return (
     <div className="product-card">
-      {/* <h1>{ JSON.stringify(product) }</h1> */}
-      <h3>{ product.name} </h3>
-      <p>Categorie: { product.category.name }</p>
-      <h4>Prix: { product.price }€</h4>
-      <FontAwesomeIcon icon={faPlus} className='add-to-cart-plus-icon' onClick={() => addToCart(product)} />
+      <FontAwesomeIcon
+        icon={faPlus}
+        className="add-to-cart-plus-icon"
+        onClick={handleAddToCart}
+      />
+      <Link to={`/product/${product.id}`}>
+        <img src={imageUrl} alt="" srcset="" />
+        <h3>{product.name} </h3>
+        <p>Categorie: {product.category.name}</p>
+        <h4>Prix: {product.price}€</h4>
+      </Link>
     </div>
   );
 };
