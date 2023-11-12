388517f12c04a0a8bd5f232797c80808821a5282
AmineAffif <affif.amine@live.fr>
2023-11-12 01:22:45 +0100

    feat: Corrected pagination and added some styling

--- src/Components/Pages/SingleProductPage.jsx
+++ src/Components/Pages/SingleProductPage.jsx
@@ -7,11 +7,12 @@ function SingleProductPage() {
   const [product, setProduct] = useState(null);
 
   useEffect(() => {
-    axios.get(`http://localhost:3000/products/${id}`)
-      .then(response => {
+    axios
+      .get(`http://localhost:3000/products/${id}`)
+      .then((response) => {
         setProduct(response.data);
       })
-      .catch(error => console.error('Error fetching product data: ', error));
+      .catch((error) => console.error("Error fetching product data: ", error));
   }, [id]);
 
   if (!product) {
@@ -19,15 +20,20 @@ function SingleProductPage() {
   }
 
   return (
-    <>
-      <div>Produit</div>
-      <div>Affichage du produit {id}</div>
-      <img src={`https://picsum.photos/400/550?random=${product.id}`} alt={product.name} />
-      <h3>{product.name}</h3>
-      <p>Catégorie: {product.category.name}</p>
-      <h4>Prix: {product.price}€</h4>
-      {/* Ajouter plus de détails du produit ici si nécessaire */}
-    </>
+    <div className="single-product-page">
+      <img
+        src={`https://picsum.photos/400/550?random=${product.id}`}
+        alt={product.name}
+      />
+      <div className="product-info-single">
+        <h3 className="single-product-title">{product.name}</h3>
+        <p className="single-product-category">{product.category.name}</p>
+        <h4 className="single-product-price">{product.price}€</h4>
+        <p className="single-product-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, labore pariatur. Deserunt modi a quod omnis, molestiae ipsa doloribus qui amet commodi rerum unde nemo asperiores quidem facilis odit magnam.</p>
+        <p className="single-product-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore commodi ut a? Fugit neque corporis, nostrum amet, odio dolor eaque perferendis alias quidem ipsa earum quibusdam tempora ex cum. Laboriosam nulla enim ducimus esse maxime natus dolore tenetur magni ut pariatur voluptatibus eos voluptatem excepturi vel cumque libero, amet corporis voluptate nostrum? Beatae minima ad, porro ducimus deserunt deleniti quae asperiores dolor pariatur unde quaerat aliquid, nam, fugit laborum sit assumenda at voluptatibus facere! Amet similique tempore quas nulla sed inventore impedit quod dolores. Ipsa expedita dolorem molestias dolorum? Aliquid possimus error iste illo sint minus neque nobis, laborum saepe.</p>
+        <p className="single-product-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit dolorem voluptatem temporibus sit accusantium odio obcaecati tempora modi aperiam iusto in quis, rerum architecto deserunt possimus ullam, velit rem soluta veniam delectus quidem?</p>
+      </div>
+    </div>
   );
 }
 
